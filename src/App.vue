01f17c3fd7a744c1ba487d0d5043bc151572bedb
Namsik Chu <namsik@n41.com>
2024-09-27 13:53:14 -0700

    directive v-if v-else

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,10 @@
 <script setup>
-import { ref, computed } from 'vue';
 import MyCounter from './components/MyCounter.vue'
-
-const count = ref(0);
-const doubleCount = computed(() => count.value * 2);
-const increment = () => {
-  count.value++;
-};
 </script>
 
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <MyCounter :count="count" :doubleCount="doubleCount" />
-  <br><br>
-  <button @click="increment">Increment</button>
+  <MyCounter />
 </template>
 
 <style>
--- src/components/MyCounter.vue
+++ src/components/MyCounter.vue
@@ -1,18 +1,25 @@
 <script setup>
-import { defineProps, onUpdated } from 'vue';
+import { ref, computed } from 'vue';
 
-const props = defineProps(['count', 'doubleCount']);
+const timer = ref(null);
+const count = ref(0);
+const doubleCount = computed(() => count.value * 2);
 
-console.log("setup: count = ", props.count);
-onUpdated(() => {
-    console.log("onUpdated: count = ", props.count);
-});
+const increment = () => count.value++;
+const start = () => {
+    timer.value = setInterval(increment, 1000);
+};
+const stop = () => {
+    clearInterval(timer.value);
+    timer.value = null;
+};
 </script>
 <template>
     <h3> MyCounter Component </h3>
     Reactive variable count: <b>{{ count }}</b> <br>
     Reactive computed variable doubleCount: <b>{{ doubleCount }}</b> <br><br>
-    Input : <input type="text" v-bind:value="count" /> <br><br>
+    <button v-if="!timer" @click="start()">Start</button>
+    <button v-else @click="stop()">Stop</button>
 </template>
 <style scoped>
 h3 {
