db2d202b3165f048ccd9a12bd436b096f2126e39
Namsik Chu <namsik@n41.com>
2024-09-30 13:30:51 -0700

    v-on with input, focus event

--- src/components/MyCounter.vue
+++ src/components/MyCounter.vue
@@ -2,6 +2,7 @@
 import { ref } from 'vue';
 
 const count = ref();
+const message = ref('');
 const verifyKey = () => {
     const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
     const moves = ['Backspace', 'ArrowLeft', 'ArrowRight', 'Delete', 'Tab', 'Home', 'End'];
@@ -14,12 +15,29 @@ const verifyKey = () => {
         event.preventDefault();
     }
 }
+const verifyMax100 = () => {
+    message.value = '';
+    // The event object is available here.
+    if (parseInt(event.target.value) > 100) {
+        message.value = 'The value must be less than or equal to 100.';
+    }
+}
+const eraseField = () => {
+    event.target.value = '';
+    count.value = '';
+    message.value = '';
+}
 </script>
 <template>
     <h3> MyCounter Component </h3>
-    Reactive variable count: <input type="text" v-model="count" @keydown="verifyKey" />
+    Reactive variable count: <input type="text" v-model="count" 
+        @keydown="verifyKey" 
+        @input="verifyMax100" 
+        @focus="eraseField" />
     <br /><br />
-    Entered value: <b>{{ count }}</b> <br>
+    Entered value: <b>{{ count }}</b>
+    <br /><br />
+    Message : <b>{{ message }}</b>
 </template>
 <style scoped>
 h3 {
